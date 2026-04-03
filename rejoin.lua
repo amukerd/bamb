4272732d4c5893bc64a89e7e156833335fd7c5f6
Amukerd <146035837+amukerd@users.noreply.github.com>
2026-04-03 00:28:03 -0400

    Update rejoin.lua

--- rejoin.lua
+++ rejoin.lua
@@ -1,12 +1,21 @@
 local TeleportService = game:GetService("TeleportService")
+local HttpService = game:GetService("HttpService")
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
+local WebhookURL = "https://discord.com/api/webhooks/1480676513668923627/c-7JOdimxEYnh3Ol2DNcCuzHyPaCrZ015TTlDnGL3aM7Rg42zRJZhFSAc3qmqNK8t51I"
 local crateListPath = workspace:WaitForChild("Components"):WaitForChild("ControlPoints"):WaitForChild("Center"):WaitForChild("capturePointUI"):WaitForChild("main"):WaitForChild("rewardFrame"):WaitForChild("crateList")
 
-local elite = crateListPath:WaitForChild("eliteCrate")
-local standard = crateListPath:WaitForChild("standardCrate")
 local premium = crateListPath:WaitForChild("premiumCrate")
+
+local function sendWebhook(message)
+    local data = {
+        ["content"] = message
+    }
+    local success, err = pcall(function()
+        HttpService:PostAsync(WebhookURL, HttpService:JSONEncode(data))
+    end)
+end
 
 local function rejoin()
     if #Players:GetPlayers() <= 1 then
@@ -19,6 +28,8 @@ local function rejoin()
 end
 
 if premium.Visible == true then
+    sendWebhook("Premium Crate!")
+
     repeat 
         task.wait(0.5) 
     until premium.Visible == false
