b9937f9301d8995bac7b78948c02ba0c6327373a
Amukerd <146035837+amukerd@users.noreply.github.com>
2026-04-06 11:34:42 -0400

    Create temp.lua

---
+++ temp.lua
@@ -0,0 +1,51 @@
+local v1 = {}
+local v2 = {
+	[1] = {
+		["Monument"] = 1
+	},
+	[2] = {
+		["Monument"] = 0.25,
+		["Apache Helipad"] = 0.25,
+		["Clocktower"] = 0.25,
+		["Eiffel Tower"] = 0.25
+	},
+	[3] = {
+		["Monument"] = 0.2,
+		["Apache Helipad"] = 0.2,
+		["Clocktower"] = 0.2,
+		["Statue of Freedom"] = 0.2,
+		["Eiffel Tower"] = 0.2
+	},
+	[6] = {
+		["Monument"] = 0.14,
+		["Apache Helipad"] = 0.14,
+		["Clocktower"] = 0.14,
+		["Statue of Freedom"] = 0.14,
+		["Executive Residence"] = 0.16,
+		["Capitol Building"] = 0.14,
+		["Eiffel Tower"] = 0.14
+	},
+	[10] = {
+		["Monument"] = 0.125,
+		["Apache Helipad"] = 0.125,
+		["Clocktower"] = 0.125,
+		["Statue of Freedom"] = 0.125,
+		["Executive Residence"] = 0.125,
+		["Capitol Building"] = 0.125,
+		["Central Office"] = 0.125,
+		["Eiffel Tower"] = 0.125
+	},
+	[15] = {
+		["Monument"] = 0.12,
+		["Apache Helipad"] = 0.11,
+		["Clocktower"] = 0.11,
+		["Statue of Freedom"] = 0.11,
+		["Executive Residence"] = 0.11,
+		["Capitol Building"] = 0.11,
+		["Central Office"] = 0.11,
+		["Eiffel Tower"] = 0.11,
+		["Observation Tower"] = 0.11
+	}
+}
+v1.LootTables = v2
+return v1
